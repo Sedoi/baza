2029e53bb045c57c32a060bdc58c4fadf0c7177c
Sedoi <sedoi.admin@gmail.com>
2016-10-29 15:45:03 +0300

    rename index to dashboard

--- test/controllers/index_controller_test.rb
+++ test/controllers/index_controller_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 class IndexControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
+  test "should get dashboard" do
     get index_index_url
     assert_response :success
   end
