ee7d6a97782f31f7b149b1b7f9795218d055e0d1
Sedoi <sedoi.admin@gmail.com>
2016-10-29 17:00:37 +0300

    change production link

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'products/index'
+  resources :products
 
   get 'dashboard/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
