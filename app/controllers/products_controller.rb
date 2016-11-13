59c6796ec7306475ff86992d302f4de8ee1671f8
Sedoi <sedoi.admin@gmail.com>
2016-11-13 13:51:10 +0200

    change controller product 2

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,9 +3,12 @@ class ProductsController < ApplicationController
     @products = Product.all.each
   end
   def new
-    if @products = Product.new(params[:product])
+    if @products == Product.create(params[:title])
+      redirect_to products_path
       # Товар успешно добавлен
     else
+      redirect_to new_product_path
+
       # Ошибка!
       end
   end
