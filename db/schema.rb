c53ff57ea0bd4ff64208be8c26e36446daa9ed31
Sedoi <sedoi.admin@gmail.com>
2017-09-26 09:44:07 +0300

    migrate to postrsql

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20161113093108) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "products", force: :cascade do |t|
     t.string   "title"
     t.string   "description"
